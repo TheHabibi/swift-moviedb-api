9d249b39650fcd9934de6afeee6bc3fd68210510
TheHabibi <celebialieren@gmail.com>
2022-06-11 13:59:55 +0200

    Initial commit

---
+++ moviedb-api-demo/moviedb_api_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  moviedb_api_demoApp.swift
+//  moviedb-api-demo
+//
+//  Created by Ali Eren Çelebi on 11.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct moviedb_api_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
